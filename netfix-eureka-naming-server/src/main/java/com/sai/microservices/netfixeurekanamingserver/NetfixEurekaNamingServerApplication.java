f2d6b807c962a82c221647e93755462512f9614e
sai kumar <ysaikumar27@gmail.com>
2020-01-05 12:19:24 +0530

    Eureka naming Server added

---
+++ netfix-eureka-naming-server/src/main/java/com/sai/microservices/netfixeurekanamingserver/NetfixEurekaNamingServerApplication.java
@@ -0,0 +1,15 @@
+package com.sai.microservices.netfixeurekanamingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class NetfixEurekaNamingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetfixEurekaNamingServerApplication.class, args);
+	}
+
+}
---
+++ netfix-eureka-naming-server/src/test/java/com/sai/microservices/netfixeurekanamingserver/NetfixEurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sai.microservices.netfixeurekanamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetfixEurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
